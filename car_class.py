86ef43e9cbb290f801b2d5b44a83be426b7c31aa
Soniel <tamboonsonieljay@gmail.com>
2026-06-04 22:23:18 +0800

    modified car_class a little bit to make it look cleaner 2

--- car_class.py
+++ car_class.py
@@ -17,20 +17,20 @@ the_car = Car(2020, "Mitsubishi")
 
 print("\n------------------------------------------------------------------------")
 
-print("\nThe car is starting....")
+print("\n\033[33mThe car is starting....\033[0m")
 print("\nCurrent speed:", the_car.get_speed(), "mph\n")
 
-print("Accelerating...")
+print("\033[32mAccelerating...\033[0m")
 for i in range(5):
     the_car.accelerate()
     print("Current speed:", the_car.get_speed())
 
-print("\nApplying the brakes...")
+print("\n\033[31mApplying the brakes...\033[0m")
 for i in range(5):
     the_car.brake()
     print("Current speed:", the_car.get_speed())
 
 print("\nFinal speed:", the_car.get_speed(), "mph")
-print("\nThe car has come to a stop.")
+print("\n\033[33mThe car has come to a stop.\033[0m")
 
 print("\n------------------------------------------------------------------------")
