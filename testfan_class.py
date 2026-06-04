3c9bb1bdb1a416f3a9d033a3264f5e78147eca20
Soniel <tamboonsonieljay@gmail.com>
2026-06-04 19:18:27 +0800

    write the test program for Fan objects and added the first fanobject with FAST speed, radius 10, color yellow, and turn on

--- testfan_class.py
+++ testfan_class.py
@@ -39,3 +39,9 @@ class Fan:
         print ("Color:", self.__color)
         print ("On:", self.__on)
         print ()
+
+fan1 = Fan()
+fan1.set_speed(Fan.FAST)
+fan1.set_radius(10)
+fan1.set_color("yellow")
+fan1.set_on(True)
