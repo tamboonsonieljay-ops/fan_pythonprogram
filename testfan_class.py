444cc1c188416707ab56e34f025f02003009ba60
Soniel <tamboonsonieljay@gmail.com>
2026-06-04 18:52:47 +0800

    added the contructor

--- testfan_class.py
+++ testfan_class.py
@@ -3,3 +3,10 @@ class Fan:
     MEDIUM = 2
     FAST = 3
 
+    def __init__(self, speed=SLOW, radius=5.0, color="blue", on=False):
+        self.__speed = speed
+        self.__radius = radius
+        self.__color = color
+        self.__on = on
+    
+
