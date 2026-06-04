b392005598b3f8fd7eff1d01d2e4bb6895a890a3
Soniel <tamboonsonieljay@gmail.com>
2026-06-04 21:34:43 +0800

    On this commit I added the Accessor Methods or the getters.

--- pet_class.py
+++ pet_class.py
@@ -12,3 +12,12 @@ def set_animal_type(self, animal_type):
 
 def set_age(self, age):
     self.__age = age
+
+def get_name(self):
+    return self.__name
+
+def get_animal_type(self):
+    return self.__animal_type
+
+def get_age(self):
+    return self.__age
