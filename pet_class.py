2e2f5ffe2be078c29083df9b98e158aaedd5265a
Soniel <tamboonsonieljay@gmail.com>
2026-06-04 21:31:11 +0800

    The last commit should be a class named 'Pet' instead of Car. On this commit I added the Mutator Methods or the setters.

--- pet_class.py
+++ pet_class.py
@@ -4,3 +4,11 @@ class Pet:
         self.__animal_type = ""
         self.__age = 0
 
+def set_name(self, name):
+    self.__name = name
+
+def set_animal_type(self, animal_type):
+    self.__animal_type = animal_type
+
+def set_age(self, age):
+    self.__age = age
