1b1e9eb86e6af04412a101ef644df46afb1a14ad
Soniel <tamboonsonieljay@gmail.com>
2026-06-04 19:02:15 +0800

    added setters

--- testfan_class.py
+++ testfan_class.py
@@ -21,3 +21,14 @@ class Fan:
     def is_on(self):
         return self.__on
 
+    def set_speed(self,speed):
+        self.__speed = speed
+    
+    def set_radius(self,radius):
+        self.__radius = radius
+
+    def set_color(self,color):
+        self.__color = color
+
+    def set_on(self,on):
+        self.__on = on
